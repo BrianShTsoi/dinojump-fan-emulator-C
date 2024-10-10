d668fd5941489fe50b73ec444c07fa37ea76a8fc
Brian Tsoi <brian.s.tsoi@gmail.com>
2024-10-09 21:42:58 -0400

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,72 @@
+// src/main.c
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+
+#define DINO_HEIGHT 20
+#define DINO_WIDTH 23
+
+const int BUF_LEN = 4096;
+const int WIDTH = 96;
+const int HEIGHT = 36;
+
+int TREX[DINO_HEIGHT][DINO_WIDTH] =  {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
+    {0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0},
+    {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+};
+
+int main() {
+    // move cursor to top left, clear everything after and below cursor
+    printf("\x1b[H\x1b[J\n");
+    while (true) {
+        for (int dino_x = 0; dino_x < WIDTH / 2; dino_x++) {
+            for (int x = 0; x < WIDTH; x++) {
+                char buf[BUF_LEN];
+                memset(buf, 0, sizeof(buf));
+
+                // // set background color as black
+                // sprintf(buf + strlen(buf), "\x1b[48;2;0;0;0m");
+
+                // hide cursor
+                sprintf(buf + strlen(buf), "\x1b[?25l");
+
+                for (int y = 0; y < HEIGHT; y++) {
+                    if (y < DINO_HEIGHT && x - dino_x < DINO_WIDTH && x - dino_x >= 0 && TREX[y][x - dino_x]) {
+                        sprintf(buf + strlen(buf), "\x1b[%i;%iH", y + 1, x + 1);
+                        sprintf(buf + strlen(buf), "\x1b[38;2;%d;%d;%dm█", 0, 255, 0);
+                    } else {
+                        sprintf(buf + strlen(buf), "\x1b[%i;%iH", y + 1, x + 1);
+                        sprintf(buf + strlen(buf), " ");
+                    }
+                }
+
+
+                printf("%s", buf);
+                fflush(stdout);
+                usleep(10);
+            }
+        }
+
+    }
+    return 0;
+}
+
